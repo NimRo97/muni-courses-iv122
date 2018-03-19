7b03db876dd8c434c62355e6ea6f4ec01f57cf69
Imrich Nagy <xnagy1@fi.muni.cz>
2018-03-19 16:05:17 +0100

    Output pictures for lesson 1

--- code/1_B.py
+++ code/1_B.py
@@ -2,6 +2,13 @@ from PIL import Image
 from SVG import Svg
 from math import sin, cos, radians
 
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+def cf_black(x1, y1, x2, y2, i, line_count):
+    return BLACK
+    
 def test_picture(n):
 
     im = Image.new("RGB", (n, n), (255, 255, 255))
@@ -15,7 +22,7 @@ def star_quadrant(im, length, start, end, line_count, color_f):
 
     x1, y1, x2, y2 = start
     for i in range(line_count):
-        r, g, b = color_f(x1, y1, x2, y2, i, length)
+        r, g, b = color_f(x1, y1, x2, y2, i, line_count)
         rgb = "rgb(" + str(r) + ", " + str(g) + ", " + str(b) + ")"
         im.line(x1, y1, x2, y2, rgb)
         x1 += (end[0] - start[0]) / line_count
@@ -23,8 +30,7 @@ def star_quadrant(im, length, start, end, line_count, color_f):
         x2 += (end[2] - start[2]) / line_count
         y2 += (end[3] - start[3]) / line_count
 
-def star(length, n, line_count,
-         color_f=lambda x1, y1, x2, y2, i, length: (0, 0, 0)):
+def star(length, n, line_count, color_f=cf_black):
 
     im = Svg("star.svg")
     for i in range(n):
