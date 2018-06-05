749d5d996ce4d734a369abb0ab13de664ac8921e
Imrich Nagy <xnagy1@fi.muni.cz>
2018-06-05 12:23:10 +0200

    finalizetion for exam

--- code/10/10.py
+++ code/10/10.py
@@ -1,8 +1,1 @@
-def parse():
-    inp = open("linreg.txt", "r")
-    out = open("out.csv", "w")
-    for line in inp:
-        data = line.strip().split(" ")
-        out.write(data[0] + ";" + data[1] + "\n")
-    inp.close()
-    out.close()
+
